e949378e7189412a0b76431cc3e0070dee163b9a
Timofey Kondrashov <tkondra1@its.jnj.com>
2018-10-21 16:45:25 -0400

    changes

--- api/controller.js
+++ api/controller.js
@@ -130,11 +130,6 @@ exports.reserve = function(req, res) {
 			"type": "required"
 		}];
 
-		console.log("blah blah blah");
-		console.log(myJson);
-		console.log(myJson.value);
-		console.log("blah blah blah23523523");
-
 		for (let attendee of myJson.value)
 		{
 			invites.push({
